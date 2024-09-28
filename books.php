c6d5c987a818d4bcb340c474fcb3465de638b688
miwanoff <marina.iva.noffa@gmail.com>
2024-09-28 14:19:29 +0300

    Add uasort

--- books.php
+++ books.php
@@ -13,3 +13,35 @@ print_r($books);
 foreach ($books as $name => $book) { // виводимо масив
     echo "$book[0]: \"$name\", $book[1]\n";
 }
+
+function year($a,$b)
+{ // callback-функція, яка визначає спосіб сортування (по рокам)
+  if ($a[1] < $b[1])
+    return -1;
+  elseif ($a[1]==$b[1])
+    return 0;
+  else
+    return 1;
+}
+echo "По року \n";
+uasort($books,"year"); // сортуємо масив за допомогою функції cmp
+
+foreach ($books as $name => $book) { // виводимо масив
+    echo "$book[0]: \"$name\", $book[1]\n";
+}
+
+function author($a,$b)
+{ // callback-функція, яка визначає спосіб сортування (по рокам)
+  if ($a[0] < $b[0])
+    return -1;
+  elseif ($a[0]==$b[0])
+    return 0;
+  else
+    return 1;
+}
+echo "По автору \n";
+uasort($books,"author"); // сортуємо масив за допомогою функції cmp
+
+foreach ($books as $name => $book) { // виводимо масив
+    echo "$book[0]: \"$name\", $book[1]\n";
+}
