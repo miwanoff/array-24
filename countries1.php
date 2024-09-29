287565808dfe95ab4ce37b7e44356463d430662e
miwanoff <marina.iva.noffa@gmail.com>
2024-09-29 13:10:51 +0300

    Add foreach

---
+++ countries1.php
@@ -0,0 +1,53 @@
+<?php
+
+$countries = [
+    [
+        "name" => "France",
+        "capital" => "Paris",
+        "area" => 640679,
+        "population" => [
+            "2000" => 59278000,
+            "2010" => 59278000,
+        ],
+    ],
+    [
+        "name" => "England",
+        "capital" => "London",
+        "area" => 130395,
+        "population" => [
+            "2000" => 58800000,
+            "2010" => 63200000,
+        ],
+    ],
+    [
+        "name" => "Deutschland",
+        "capital" => "Berlin",
+        "area" => 357021,
+        "population" => [
+            "2000" => 82260000,
+            "2010" => 81752000,
+        ],
+    ],
+];
+
+for ($i = 0; $i < count($countries); $i++) {
+    echo "{$countries[$i]['name']} : {$countries[$i]['capital']} area: {$countries[$i]['area']}  2000: {$countries[$i]['population']['2000']} 2010: {$countries[$i]['population']['2010']}\n";
+}
+
+echo "\n";
+
+foreach ($countries as $i => $country) {
+    $number = $i+1;
+    echo "$number ";
+    foreach ($country as $key => $value) 
+    {
+      if (!is_array($value))
+        echo "$key: $value; ";
+      else {
+        echo "$key: ";
+        foreach ( $value as $k => $v )
+          echo "[$k - $v]";
+        }
+     }
+     echo ";\n";
+  }
